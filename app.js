dfcfb1355bcc0ea40fc19b410afb35998c081cea
AnsiaHalm <dolgaya.nastya@gmail.com>
2022-06-02 20:25:21 +0300

    refactor: delite id to BEM in HTML according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("add-item__new-task");//Add a new task.
+var taskInput=document.querySelector(".add-item__new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("todo__list");//ul of #todo__list
-var completedTasksHolder=document.getElementById("completed__list");//completed__list
+var incompleteTaskHolder=document.querySelector(".todo__list");//ul of #todo__list
+var completedTasksHolder=document.querySelector(".completed__list");//completed__list
 
 
 //New task list item
